976502b68b9b5f2bfaafd0a4176b88da25ac9fb8
GitOnion <b93703028@ntu.edu.tw>
2016-07-18 12:34:35 -0700

    push testTask to server side

---
+++ log_process.py
@@ -0,0 +1,8 @@
+from computer import login_server
+
+credentials = {'username': 'jongkai', 'domain': 'dmas.berkeley.edu'}
+roots = {'data': '~/data', 'src': '~/src'}
+
+server = login_server.LoginServer(('berkeley', 'shackleton'), 20, roots, credentials)
+server.connect()
+server.start_process('python testTask.py')
---
+++ testTask.py
@@ -0,0 +1,5 @@
+import time
+
+for i in range(100):
+    time.sleep(2)
+    print('testResult' + str(i))
